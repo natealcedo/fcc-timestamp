5133e0f1b9fc832764616edbf6e52c442156d5bd
nathaniel d alcedo jr <ndaljr@gmail.com>
2016-12-17 14:54:41 +0800

    Added a little padding to the main api page

--- routes/index.js
+++ routes/index.js
@@ -8,18 +8,18 @@ router.get('/', function (req, res, next) {
 });
 
 
-router.get('/:timestamp', (req,res) => {
+router.get('/:timestamp', (req, res) => {
   let time = moment(req.params.timestamp, 'MMMM DD, YYYY', true);
   if (!time.isValid())
     time = moment.unix(req.params.timestamp);
-  
+
   if (!time.isValid()) {
     res.json({
       'humanReadable': null,
       'unix': null
     });
   }
-  
+
   res.json({
     'humanReadable': time.format('MMMM DD, YYYY'),
     'unix': time.format('X')
