d177f11dcb7d08c665743440c94c89a948a9af84
nathaniel d alcedo jr <ndaljr@gmail.com>
2016-12-17 14:04:24 +0800

    Second Commit. Finished App

--- app.js
+++ app.js
@@ -6,7 +6,6 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var index = require('./routes/index');
-var users = require('./routes/users');
 
 var app = express();
 
@@ -23,7 +22,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
-app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -43,4 +41,6 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
-module.exports = app;
+app.listen(3000, function(){
+  console.log('Server Started on port 3000')
+});
--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,30 @@
 var express = require('express');
 var router = express.Router();
+var moment = require('moment')
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'TimeStamp Microservice' });
 });
 
 
-router.get('/:time',function(req,res){
-  var data = {time: req.params.time};
-  res.json(data);
+router.get('/:timestamp', (req,res) => {
+  let time = moment(req.params.timestamp, 'MMMM DD, YYYY', true);
+  if (!time.isValid())
+    time = moment.unix(req.params.timestamp);
+  
+  if (!time.isValid()) {
+    res.json({
+      'humanReadable': null,
+      'unix': null
+    });
+  }
+  
+  res.json({
+    'humanReadable': time.format('MMMM DD, YYYY'),
+    'unix': time.format('X')
+  });
 });
+
+
 module.exports = router;
