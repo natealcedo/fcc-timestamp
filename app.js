fdedea3c5925c404cfd2170831a8083f25ce3260
nathaniel d alcedo jr <ndaljr@gmail.com>
2016-12-17 14:43:10 +0800

    Change port number

--- app.js
+++ app.js
@@ -41,6 +41,4 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
-app.listen(3000, function(){
-  console.log('Server Started on port 3000')
-});
+app.listen(process.env.PORT || 5000);
